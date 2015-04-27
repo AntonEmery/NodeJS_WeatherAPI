a8e8555c016b025552d0d29faa12211bdf15bec4
AntonEmery <antonemery@gmail.com>
2015-04-26 22:31:44 -0700

    returning chunk from api call

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var https = require('https');
 
 
 function getWeather() {
-	https.get(
-		'https://api.forecast.io/forecast/5a6971beaded415ef73e686e73f6be02/45.525121, 122.689441', 
+	var weather = https.get(
+		'https://api.forecast.io/forecast/5a6971beaded415ef73e686e73f6be02/45.525121, -122.689441', 
 		function(response) {
-			console.log('Result' + response.statusCode);
-		})
+			response.on('data', function(chunk){
+			console.log(chunk);
+			})
+			console.log('Result ' + response.statusCode);
+		}).on('error', function(error) {
+			console.log('Got error ' + error.message);
+			})
+
 };
 
 getWeather();
