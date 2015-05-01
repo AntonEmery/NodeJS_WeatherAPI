be1913c7c4cf1ffb058a58d248c16c6d80824647
AntonEmery <antonemery@gmail.com>
2015-05-01 07:14:44 -0700

    need to get lat lng into weather function

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var data = "";
 var zipData = "";
 
 
+
 function getWeather() {
 	var weather = https.get(
-		'https://api.forecast.io/forecast/5a6971beaded415ef73e686e73f6be02/45.525121, -122.689441', 
+		'https://api.forecast.io/forecast/5a6971beaded415ef73e686e73f6be02/39.9129412, -104.7956055', 
 		function(response) {
 			//as data comes in chunks, adds it to the data var
 			response.on('data', function(chunk){  
@@ -19,8 +20,6 @@ function getWeather() {
 				var body = JSON.parse(data);
 				//print out the weather summary
 				console.log(body.currently.summary);
-				//logs the node envir and the name of the file
-				//console.log(process.argv);
 			})
 
 			//logs result or error
@@ -46,9 +45,13 @@ function getZip() {
 				//logging lat and lng
 				console.log(zipResult.results[0].geometry.location.lat);
 				console.log(zipResult.results[0].geometry.location.lng);
+				//need to get these variables into the getWeather function
+				var zipLat = zipResult.results[0].geometry.location.lat;
+				var zipLng = zipResult.results[0].geometry.location.lng;
 				})
 				console.log('Result ' + response.statusCode);
 			})
+		getWeather();
 	}
 
 
